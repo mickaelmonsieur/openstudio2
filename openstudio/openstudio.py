5691e0c0c93a3a5c6607e22555a809a7ad3a2791
Mickael <mickael.monsieur@gmail.com>
2018-02-08 23:36:56 +0100

    vst dll/so + ajout artist db

--- openstudio/openstudio.py
+++ openstudio/openstudio.py
@@ -5,6 +5,7 @@ import wx
 import openstudioAudio
 import openstudioGUI
 import sqlite3 as lite
+from mutagen.mp3 import MP3
 
 #inherit from the MainFrame created in wxFowmBuilder and create MainFrame
 class MainFrame(openstudioGUI.MainFrame):
@@ -15,10 +16,10 @@ class MainFrame(openstudioGUI.MainFrame):
         print (sys.version)
         self.audioWrapper_1 = openstudioAudio.openstudioAudioInstance(self)
         self.con = lite.connect('openstudio.db')
-        self.songsColumn1 = self.songs.AppendTextColumn( u"ID", 0, width=50 )
+        self.songsColumn1 = self.songs.AppendTextColumn( u"ID", 0, width=70 )
         self.songsColumn2 = self.songs.AppendTextColumn( u"Artist", 1, width=400 )
         self.songsColumn3 = self.songs.AppendTextColumn( u"Title", 2, width=400 )
-        self.songsColumn4 = self.songs.AppendTextColumn( u"Duration", 3, width=50 )
+        self.songsColumn4 = self.songs.AppendTextColumn( u"Duration", 3, width=70 )
         self.getSongs()
 
     def play(self,event):
@@ -64,8 +65,9 @@ class MainFrame(openstudioGUI.MainFrame):
 
                 if row == None:
                     break
-
-                self.songs.AppendItem( [str(row[0]),row[1],row[2],str(row[3]) ])
+                m, s = divmod(int(row[3]), 60)
+                h, m = divmod(m, 60)
+                self.songs.AppendItem( [str(row[0]),row[1],row[2],"%02d:%02d:%02d" % (h, m, s) ])
 
     def importFile(self,event):
         frame = ImportFrame(None)
@@ -82,11 +84,35 @@ class ImportFrame(openstudioGUI.ImportFrame):
         self.con = lite.connect('openstudio.db')
 
     def importFile(self,event):
+        if self.filePicker.GetPath():
+            audio = MP3(self.filePicker.GetPath())
+            artist = str(audio["TPE1"])
+            cur = self.con.cursor()
+            artistId=self.getArtistId(artist)
+
+            if not artistId:
+                print artist
+                print type(artist)
+                cur.execute("""INSERT INTO artist(name) VALUES(?)""", (artist,))
+                artistId = cur.lastrowid
+                print('Artist created with Id: %d' % artistId)
+            else:
+                print('Artist exists with Id: %d' % artistId)
+
+            cur.execute("""INSERT INTO song(artistId,title,duration,filename) VALUES(?, ?, ?, ?)""", (artistId, str(audio["TIT2"]), "%.2f" % audio.info.length, self.filePicker.GetPath()))
+            id = cur.lastrowid
+            print('Song created with Id: %d' % id)
+            self.con.commit()
+        else:
+            print "No file selected!"
+
+    def getArtistId(self, name):
         cur = self.con.cursor()
-        cur.execute("""INSERT INTO song(artistId,title,duration,filename) VALUES(?, ?, ?, ?)""", (1, "test", 200, self.filePicker.GetPath()))
-        id = cur.lastrowid
-        print('Created with Id: %d' % id)
-        self.con.commit()
+        cur.execute('SELECT artistId FROM artist WHERE name=?', (name,))
+        artistId = cur.fetchone()
+        artistId = artistId and artistId[0] or False
+        return artistId
+
 
 #mandatory in wx, create an app, False stands for not deteriction stdin/stdout
 #refer manual for details
